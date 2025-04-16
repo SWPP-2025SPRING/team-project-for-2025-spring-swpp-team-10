e57094fdaf11c12be8129d8ce01b2c9e9ff86387
honey766 <honey76684@gmail.com>
2025-04-17 02:52:47 +0900

    add PressureSensitivePlatform.cs
    플레이어가 밟으면 아래로 내려가고, 떠나면 다시 올라오는 플랫폼 스크립트 추가

---
+++ example/Rope Action Practice/assets/Scripts/Platform/PressureSensitivePlatform.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+// 플레이어가 밟으면 아래로 내려가고, 떠나면 다시 올라오는 플랫폼
+public class PressureSensitivePlatform : MonoBehaviour
+{
+    [Header("Values")]
+    [Tooltip("아래로 내려가는 최대 길이(양수)")]
+    [SerializeField] private float maxDownLength = 20f;
+    [Tooltip("플랫폼이 움직이는 속도")]
+    [SerializeField] private float speed = 5f;
+    [Tooltip("속도가 목표값으로 얼마나 빠르게 수렴하는지 조절")]
+    [SerializeField] private float lerpSpeed = 2f;
+
+    
+    [Header("Dotted 매테리얼을 할당해 주세요\n미할당 시 플랫폼 이동 범위가 표시되지 않습니다.")]
+    [SerializeField] private Material dotted;
+
+    private float initY; // 오브젝트의 원래 y 좌표
+    private bool onPlayer; // 플레이어가 위에 있으면 true
+    private float collisionVelocityRatio = 0.4f; // 충돌했을 때 플랫폼이 플레이어의 속도를 전달받는 정도'
+
+    private Rigidbody rb;
+    private Transform cylinder;
+
+
+
+    void Start()
+    {
+        initY = transform.position.y;
+        onPlayer = false;
+
+        rb = GetComponent<Rigidbody>();
+
+        // 플랫폼이 움직이는 범위 표시
+        if (dotted != null) {
+            cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder).transform;
+            cylinder.gameObject.name = "PlatformMovementBoundary"; 
+            SetCylinder();
+        }
+    }
+
+
+    void Update()
+    {
+        if (onPlayer) {
+            Down();
+        }
+        else {
+            Up();
+        }
+    }
+
+    private void Down()
+    {
+        if (transform.position.y <= initY - maxDownLength) {
+            transform.position = new Vector3(transform.position.x, initY - maxDownLength, transform.position.z);
+            rb.velocity = Vector3.zero;
+            return;
+        }
+        
+        rb.velocity = Vector3.up * Mathf.Lerp(rb.velocity.y, -speed, lerpSpeed * Time.deltaTime);
+    }
+
+    private void Up()
+    {
+        if (transform.position.y >= initY) {
+            transform.position = new Vector3(transform.position.x, initY, transform.position.z);
+            rb.velocity = Vector3.zero;
+            return;
+        }
+        
+        rb.velocity = Vector3.up * Mathf.Lerp(rb.velocity.y, speed, lerpSpeed * Time.deltaTime);
+    }
+
+
+    private void SetCylinder()
+    {
+        // 콜라이더 켜져 있으면 끄기
+        cylinder.GetComponent<Collider>().enabled = false;
+
+        // Transform값 설정
+        cylinder.position = new Vector3(transform.position.x, initY - maxDownLength * 0.5f, transform.position.z);
+        cylinder.localScale = new Vector3(0.2f, 1 + maxDownLength * 0.5f, 0.2f);
+        cylinder.rotation = Quaternion.identity;
+
+        // Dotted 매테리얼의 점선 간격 조정
+        Renderer rd = cylinder.GetComponent<Renderer>();
+        rd.material = dotted;
+        // 0~5 => 2, 5~10 => 3, 10~15 => 4
+        int cnt = 1 + Mathf.Max(0, Mathf.CeilToInt(maxDownLength / 5f));
+        rd.material.SetFloat("_scale", cnt);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
+            onPlayer = true;
+            // 충돌할 때 플레이어의 y축 속도 반영
+            rb.velocity += Vector3.up * Mathf.Min(0, collision.relativeVelocity.y) * collisionVelocityRatio;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
+            onPlayer = false;
+        }
+    }
+}
